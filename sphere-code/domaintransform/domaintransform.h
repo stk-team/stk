af446262afbb5e7a2a8d0db32ce14ee3886f2b53
sinbag <gurpritsbagga@gmail.com>
2015-11-20 20:54:52 -0500

    removing old version

--- sphere-code/domaintransform/domaintransform.h
+++
@@ -1,9 +0,0 @@
-#ifndef SAMPLING_H
-#define SAMPLING_H
-
-void concentricSampleDisk(double u1, double u2, double *dx, double *dy);
-void uniformSampleHemiSphere(double u1, double u2, double* dx, double* dy, double* dz);
-void uniformSampleSphere(double u1, double u2, double* dx, double* dy, double* dz);
-void cosineSampleHemiSphere(double u1, double u2, double* dx, double* dy, double* dz);
-
-#endif // CORE_H
